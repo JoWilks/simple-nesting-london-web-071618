4e9abac652ae17022d75421b79edbc58d0d09a77
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-04 11:02:05 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -130,5 +130,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:known_for]<< "Assembly"
+programmer_hash[:dennis_ritchie][:known_for][1]<< "Assembly"
 end
